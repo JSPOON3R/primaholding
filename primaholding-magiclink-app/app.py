b78ca3ff267b9dc0e732b4f0a766499a8f8af606
John Spooner <127133215+JSPOON3R@users.noreply.github.com>
2025-07-10 12:31:12 +0200

    Update app.py

--- primaholding-magiclink-app/app.py
+++ primaholding-magiclink-app/app.py
@@ -38,6 +38,6 @@ def generate_link():
     }
 
     magic_link = jwt.encode(payload, REST_KEY, algorithm="HS256")
-    magic_url = f"[space_url]?magic_link={magic_link}&session_id={session_id}"
+    magic_url = f"{space_url}?magic_link={magic_link}&session_id={session_id}"
 
     return jsonify({"message": "Magic Link", "link": magic_url})
